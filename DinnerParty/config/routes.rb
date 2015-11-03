d15dbf3bd46cea5e9339692d3a0fa199b58a2773
talum <tracidini@gmail.com>
2015-11-03 09:58:20 -0500

    Add routes for dinner

--- DinnerParty/config/routes.rb
+++ DinnerParty/config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   get 'auth/:facebook/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy', as: 'logout'
   #resources :sessions, only: [:create, :destroy]
-
+  resources :dinners
 end
