d27725feb6c86e2855b7d958b6c2f90cbaedb602
talum <tracidini@gmail.com>
2015-11-04 17:11:52 -0500

    Reverts current_user

--- DinnerParty/app/controllers/application_controller.rb
+++ DinnerParty/app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   helper_method :logged_in, :current_user
 
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    @user ||= User.find_by(id: session[:user_id])
   end
 
   def logged_in
